80b2e95d55ba2df6eecdafe5f9d1d6894d364fc4
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2023-02-21 08:10:52 -0300

    Override network mtu value

--- pkg/compose/create.go
+++ pkg/compose/create.go
@@ -40,6 +40,7 @@ import (
 	"github.com/docker/go-units"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 
 	"github.com/compose-spec/compose-go/types"
 
@@ -1152,6 +1153,19 @@ func (s *composeService) ensureNetwork(ctx context.Context, n types.NetworkConfi
 			}
 			createOpts.IPAM.Config = append(createOpts.IPAM.Config, config)
 		}
+
+		// override MTU value and set custom MTU one.
+		// This is required for gitpod.io due to the veth change
+		// https://github.com/gitpod-io/gitpod/pull/8955
+		if createOpts.Options == nil {
+			createOpts.Options = make(map[string]string)
+		}
+
+		netIface, err := netlink.LinkByName("ceth0")
+		if err == nil {
+			createOpts.Options["com.docker.network.driver.mtu"] = fmt.Sprintf("%v", netIface.Attrs().MTU)
+		}
+
 		networkEventName := fmt.Sprintf("Network %s", n.Name)
 		w := progress.ContextWriter(ctx)
 		w.Event(progress.CreatingEvent(networkEventName))
